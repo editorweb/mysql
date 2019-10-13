3f1d26c0436b2e041025c8da176648e50ac6b563
editorweb <jorge.r@msn.com>
2019-10-13 07:53:35 -0500

    Create prueba.php

---
+++ prueba.php
@@ -0,0 +1,39 @@
+<?php
+include("config.php");
+$query = "SELECT * FROM usuarios";
+print("<table>");
+$resultado=$mysqli->query($query);
+foreach ($resultado as $rows) { 
+$query = "SELECT * FROM amigos WHERE id1=".$rows["id"]." OR id2=".$rows["id"];
+$amigos=$mysqli->query($query);
+print("<tr>");
+print("<td>".$rows["id"]."</td>");
+print("<td>".$rows["nombre"]."</td>");
+print("<td>".$rows["telefono"]."</td>");
+print("<td>".$rows["email"]."</td>");
+print("<td>");
+while ($friend = $amigos->fetch_assoc()) {
+	$query = "SELECT * FROM usuarios WHERE id=".$friend["id1"];
+  $fish=$mysqli->query($query);
+  $oreo = $fish->fetch_assoc();
+ 	if($oreo["id"]!=$rows["id"]){
+	$otreo = $oreo["nombre"];
+	print($otreo);
+	print("<br>");
+	}
+	$query = "SELECT * FROM usuarios WHERE id=".$friend["id2"];
+  $fish=$mysqli->query($query);
+  $oreo = $fish->fetch_assoc();
+ 	if($oreo["id"]!=$rows["id"]){
+	$otreo = $oreo["nombre"];
+	print($otreo);
+	print("<br>");
+	}
+}
+print("</td>");
+print("</tr>"); 
+}
+print("</table>");
+$resultado->free();
+$mysqli->close();
+?>
